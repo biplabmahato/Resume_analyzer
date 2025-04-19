1fbf2326c87bdb3de6994177bc71251a21fed568
Biplab Mahato <biplab25@users.noreply.huggingface.co>
2025-04-19 10:25:13 +0000

    Update app.py

--- app.py
+++ app.py
@@ -110,5 +110,3 @@ if uploaded_file:
                 st.error(f"Analysis failed: {e}")
 
 #Footer
-st.markdown("---")
-st.markdown("""<p style= 'text-align: center;' >Powered by <b>Streamlit</b> and <b>Google Gemini AI</b> | Developed by <a href="https://www.linkedin.com/in/dutta-sujoy/"  target="_blank" style='text-decoration: none; color: #FFFFFF'><b>Sujoy Dutta</b></a></p>""", unsafe_allow_html=True)
